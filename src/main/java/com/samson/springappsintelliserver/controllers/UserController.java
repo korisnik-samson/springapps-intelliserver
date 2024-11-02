e4d92175f6fd80bc1e59e75a2f10c77dc4b4ce55
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2024-11-02 18:00:02 +0100

    Update UserController.java

--- src/main/java/com/samson/springappsintelliserver/controllers/UserController.java
+++ src/main/java/com/samson/springappsintelliserver/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.samson.springappsintelliserver.controllers;
 import com.samson.springappsintelliserver.models.Users;
 import com.samson.springappsintelliserver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
