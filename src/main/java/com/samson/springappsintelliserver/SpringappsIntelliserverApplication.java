6d0f67ca09246df5e1976c46436f8d0e93be42ad
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2024-11-02 04:39:02 +0100

    Initial Commit

---
+++ src/main/java/com/samson/springappsintelliserver/SpringappsIntelliserverApplication.java
@@ -0,0 +1,13 @@
+package com.samson.springappsintelliserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappsIntelliserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappsIntelliserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samson/springappsintelliserver/SpringappsIntelliserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samson.springappsintelliserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappsIntelliserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
