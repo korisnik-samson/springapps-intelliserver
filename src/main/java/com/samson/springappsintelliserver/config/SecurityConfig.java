840f9d793eb85f9e1dc1d47b8469b7a0adbac459
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2024-11-05 09:27:59 +0100

    Update SecurityConfig.java

--- src/main/java/com/samson/springappsintelliserver/config/SecurityConfig.java
+++ src/main/java/com/samson/springappsintelliserver/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.samson.springappsintelliserver.config;
 
-import com.samson.springappsintelliserver.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +15,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
