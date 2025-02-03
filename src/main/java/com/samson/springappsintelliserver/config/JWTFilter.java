45622c24a1770153f6037ffe6ae5da707ac2a6c4
Samson <126519078+korisnik-samson@users.noreply.github.com>
2025-02-03 10:49:51 +0100

    Update JWTFilter.java

--- src/main/java/com/samson/springappsintelliserver/config/JWTFilter.java
+++ src/main/java/com/samson/springappsintelliserver/config/JWTFilter.java
@@ -2,14 +2,12 @@ package com.samson.springappsintelliserver.config;
 
 import com.samson.springappsintelliserver.services.JWTService;
 import com.samson.springappsintelliserver.services.MyUserDetailsService;
-import com.samson.springappsintelliserver.utils.Utils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -53,7 +51,8 @@ public class JWTFilter extends OncePerRequestFilter {
 
             if (jwtService.validateToken(token, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
+                        userDetails, null, userDetails.getAuthorities()
+                );
 
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
